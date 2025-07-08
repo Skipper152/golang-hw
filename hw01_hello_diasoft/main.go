5de3e04f512c5860c8d76310eafa543607a4aa18
Николай Штрауб <nshtraub@diasoft.ru>
2025-07-08 17:55:59 +0300

    hw01_hello_diasoft

--- hw01_hello_diasoft/main.go
+++ hw01_hello_diasoft/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/hello/reverse"
+)
+
 func main() {
-	//
+	text := "Hello, DIASOFT!"
+
+	reverseText := reverse.String(text)
+
+	fmt.Println(reverseText)
 }
